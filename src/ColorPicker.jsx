3642bea66acfd192667aac334ddf398d13a685a2
Tamtai <107045052+Tainyz@users.noreply.github.com>
2025-04-14 14:45:05 +0700

    add function and css styles

--- src/ColorPicker.jsx
+++ src/ColorPicker.jsx
@@ -1,8 +1,21 @@
+import { useState } from "react"
 
 
 const ColorPicker = () => {
+    const [color,setColor] = useState("#FFFFFF");
+    function handleColorChange(e){
+        setColor(e.target.value);
+    }
+
   return (
-    <div>ColorPicker</div>
+    <div className="color-picker-container">
+        <h1>Color Picker</h1>
+        <div className="color-display" style={{backgroundColor: color}}>
+            <p>Selected Color: {color}</p>
+        </div>
+        <label htmlFor="">Select a Color:</label>
+        <input type="color" name="" value={color} onChange={handleColorChange} />
+    </div>
   )
 }
 
